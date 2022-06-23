42590e0bb4edb9e900a7c772126499b1b052eb03
TMoraesTI <89168317+TMoraesTI@users.noreply.github.com>
2022-06-23 11:19:59 -0300

    Add files via upload

--- Simulador/script.js
+++ Simulador/script.js
@@ -111,7 +111,7 @@ function calcularMediaFinal1(){
     var Parte2 = parseFloat(document.getElementById('Parte2').value.replace(",",".").replace("","0"));
 
 
-    var soma = (Parte1 + Parte2) /2;
+    var soma = Parte1 + Parte2;
     var somaDecimal = Math.trunc(soma * 100) / 100;
 
     document.getElementById('resultadoMediaFinal1').value = somaDecimal.toString().replace(".", ","); 
@@ -126,10 +126,10 @@ function calcularMediaFinal1(){
         document.getElementById('situacaoMediaFinal1').value = "Aprovado";
         document.getElementById('situacaoMediaFinal1').style.color = "#55cb50";
     }
-    else if (resultadoDecimal <= 60.99 && resultadoDecimal >= 40) {
-        document.getElementById('situacaoMediaFinal1').value = ("Alcançar na AS (" + (12 - resultadoDecimal) + ")").replace(".",",");;
-        document.querySelector('#divisaoAVS').style.display = "grid";
-        document.querySelector('#divisaoAVSSituacao').style.display = "grid";
+    else if (resultadoDecimal <= 69.99 && resultadoDecimal >= 40) {
+        document.getElementById('situacaoMediaFinal1').value = ("Alcançar no Exame Final (" + (100 - resultadoDecimal) + ")").replace(".",",");;
+        document.querySelector('#divisaoMediaFinal2').style.display = "grid";
+        document.querySelector('#divisaoMediaFinal2Situacao').style.display = "grid";
         document.getElementById('situacaoMediaFinal1').style.color = "#c2a202";
     }
     else {
@@ -139,53 +139,55 @@ function calcularMediaFinal1(){
     
     document.getElementById('mediaFinal1').disabled = true;
 }
-function calcularAVS(){
+
+function calcularMediaFinal2(){
     /*--Chamar nota da AV colocada pelo aluno ou importada automatica no evento anterior--*/
         /*mediaFinal1 = valor do campo anterior*/
     var mediaFinal1 = parseFloat(document.getElementById('resultadoMediaFinal1').value.replace(",",".").replace("","0"));
         /*mediaFinal2 = valor do campo adcionado pelo aluno*/
-    var mediaFinal2 = parseFloat(document.getElementById('mediaFinal2').value.replace(",",".").replace("","0"));
-    var AVS = parseFloat(document.getElementById('AVS').value.replace(",",".").replace("","0"));
+    var atividade8 = parseFloat(document.getElementById('atividade8').value.replace(",",".").replace("","0"));
 
 
-    var soma = (((mediaFinal1 + mediaFinal2)/2) + AVS)/2;
+    var soma = mediaFinal1 + atividade8;
     var somaDecimal = Math.trunc(soma * 100) / 100;
 
-    document.getElementById('resultadoAVS').value = somaDecimal.toFixed(2).toString().replace(".", ",");
+    document.getElementById('resultadoMediaFinal2').value = somaDecimal.toFixed(2).toString().replace(".", ",");
 
      /*--Condição da média do aluno--*/
-    var resultado = parseFloat(document.getElementById('resultadoAVS').value);
+    var resultado = parseFloat(document.getElementById('resultadoMediaFinal2').value);
     var resultadoDecimal = Math.trunc(resultado * 100) / 100;
 
-    if (resultadoDecimal >= 6) {
-        document.getElementById('situacaoAVS').value = "Aprovado";
-        document.getElementById('situacaoAVS').style.color = "#55cb50";
+    if (resultadoDecimal >= 60) {
+        document.getElementById('situacaoMediaFinal2').value = "Aprovado";
+        document.getElementById('situacaoMediaFinal2').style.color = "#55cb50";
     }
     else {
-        document.getElementById('situacaoAVS').value = "Reprovado";        
-        document.getElementById('situacaoAVS').style.color = "#c52010";
+        document.getElementById('situacaoMediaFinal2').value = "Reprovado";        
+        document.getElementById('situacaoMediaFinal2').style.color = "#c52010";
     }
 }
 
 function deletar(){
     /*--Limpar campos de médias e situação--*/
-    document.getElementById('AP1').value = "";
-    document.getElementById('AP2').value = "";
-    document.getElementById('AP3').value = "";
-    document.getElementById('AP4').value = "";
-    document.getElementById('PI').value = "";
+    document.getElementById('atividade1').value = "";
+    document.getElementById('atividade2').value = "";
+    document.getElementById('atividade3').value = "";
+    document.getElementById('atividade4').value = "";
+    document.getElementById('atividade5').value = "";
+    document.getElementById('atividade6').value = "";
+    document.getElementById('atividade7').value = "";
+    document.getElementById('atividade8').value = "";
     document.getElementById('Parte1').value = "";
     document.getElementById('Parte2').value = "";
     document.getElementById('resultadoParte1').value = "";
     document.getElementById('resultadoParte2').value = "";
-    document.getElementById('mediaFinal').value = "";
-    document.getElementById('resultadoMediaFinal').value = "";
-    document.getElementById('situacaoMediaFinal').value = "";
-    document.getElementById('AVS').value = "";
-    document.getElementById('resultadoAVS').value = "";
-    document.getElementById('situacaoAVS').value = "";
-    document.querySelector('#divisaoAVS').style.display = "none";
-    document.querySelector('#divisaoAVSSituacao').style.display = "none";
+    document.getElementById('mediaFinal1').value = "";
+    document.getElementById('resultadoMediaFinal1').value = "";
+    document.getElementById('situacaoMediaFinal1').value = "";
+    document.getElementById('resultadoMediaFinal2').value = "";
+    document.getElementById('situacaoMediaFinal2').value = "";
+    document.querySelector('#divisaoMediaFinal2').style.display = "none";
+    document.querySelector('#divisaoMediaFinal2Situacao').style.display = "none";
 }
 
 
